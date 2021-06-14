1a94f6ecec9c820341297cd12fd4b1215269e725
Amanda McGuire <aruwart001@gmail.com>
2021-06-14 15:46:14 -0600

    Update key class and spec for cracked code

--- lib/key.rb
+++ lib/key.rb
@@ -3,9 +3,20 @@ require './lib/modules/Indexable'
 class Key
   include Indexable
 
-  def initialize(key, offset)
-    @key = key
-    @offset = offset
+  attr_reader :key, :offset
+
+  def initialize(key, offset, shift)
+    @key = key == 0 ? (random_key) : (key)
+    @offset = offset == 0 ? (date_today) : (offset)
+    @shift = shift
+  end
+
+  def random_key
+    key = (0..9).to_a.sample(5).join
+  end
+
+  def date_today
+    Date.today.strftime('%d%m%y')
   end
 
   def split_key
@@ -38,5 +49,55 @@ class Key
 
   def shift_d_amount
     index_key(3, 4)
+  end
+
+  def crack_rotate_for_shift
+    @shift.crack_end_message_shift.rotate(4 - @shift.shift_message.message.length % 4)
+  end
+
+  def crack_shift_a_key
+    (crack_rotate_for_shift[0] - split_offset[0]).to_s.rjust(2, '0').split('')
+  end
+
+  def crack_shift_b_key
+    (crack_rotate_for_shift[1] - split_offset[1]).to_s.rjust(2, '0').split('')
+  end
+
+  def crack_shift_c_key
+    (crack_rotate_for_shift[2] - split_offset[2]).to_s.rjust(2, '0').split('')
+  end
+
+  def crack_shift_d_key
+    (crack_rotate_for_shift[3] - split_offset[3]).to_s.rjust(2, '0').split('')
+  end
+
+  def crack_a_key
+    crack_shift_a_key.join
+  end
+
+  def crack_b_key
+    key = (27 - ((crack_shift_a_key[1].to_i * 10) % 27)) + crack_shift_b_key.join.to_i
+    if key > 9
+      (10 - key).to_s
+    else
+      key.to_s
+    end
+  end
+
+  def crack_c_key
+    key = (27 - ((crack_shift_b_key[1].to_i * 10) % 27)) + crack_shift_c_key.join.to_i
+    if key > 9
+      (10 - key).to_s
+    else
+      key.to_s
+    end
+  end
+
+  def crack_d_key
+    crack_shift_d_key[1]
+  end
+
+  def crack_key
+    @key = crack_a_key + crack_b_key + crack_c_key + crack_d_key
   end
 end
--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -2,10 +2,21 @@ require './lib/key'
 
 RSpec.describe Key do
   before(:each) do
-    @key = Key.new('02715', '040895')
+    @shift = Shift.new('hello world', '02715', '040895')
+    @key = Key.new('02715', '040895', @shift)
   end
   it 'exists' do
     expect(@key).to be_a(Key)
+  end
+
+  it 'can generate a random key' do
+    key = Key.new(0, 0, @shift)
+    expect(key.random_key.length).to eq(5)
+  end
+
+  it 'can return todays date' do
+    key = Key.new(0, 0, @shift)
+    expect(@key.date_today).to eq('140621')
   end
 
   it 'can return string as array' do
@@ -25,5 +36,35 @@ RSpec.describe Key do
     expect(@key.shift_b_amount).to eq(27)
     expect(@key.shift_c_amount).to eq(73)
     expect(@key.shift_d_amount).to eq(20)
+  end
+
+  describe 'Cracking the key' do
+    before(:each) do
+      @shift = Shift.new('vjqtbeaweqihssi', 0, '291018')
+      @key = Key.new(0, '291018', @shift)
+    end
+
+    it 'can rotate shift to correct position' do
+      expect(@key.crack_rotate_for_shift).to eq([14, 5, 5, 8])
+    end
+
+    it 'can find key in correct shift position' do
+      expect(@key.crack_shift_a_key).to eq(['0', '8'])
+      expect(@key.crack_shift_b_key).to eq(['0', '2'])
+      expect(@key.crack_shift_c_key).to eq(['0', '3'])
+      expect(@key.crack_shift_d_key).to eq(['0', '4'])
+    end
+
+    it 'can find key by shift to combine final cracked key' do
+      expect(@key.crack_a_key).to eq('08')
+      expect(@key.crack_b_key).to eq('3')
+      expect(@key.crack_c_key).to eq('0')
+      expect(@key.crack_d_key).to eq('4')
+    end
+
+    it 'can return cracked key' do
+      @key.crack_key
+      expect(@key.key).to eq('08304')
+    end
   end
 end
